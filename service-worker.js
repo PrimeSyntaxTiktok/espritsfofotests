a15a3f82a698ea955301733b041fe7d4e5e0c8c0
PrimeSyntax <minoucoding@gmail.com>
2026-08-13 19:57:08 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const CACHE_VERSION = "2026-08-13-v15";
+const CACHE_VERSION = "2026-08-13-v16";
 const CACHE_PREFIX = "sprite-locker-";
 const SHELL_CACHE = `${CACHE_PREFIX}shell-${CACHE_VERSION}`;
 const ASSET_CACHE = `${CACHE_PREFIX}assets-${CACHE_VERSION}`;
